179bfe0e82e192ca0c88cc8f33477767220eeacd
gus <gustav1105@gmail.com>
2024-09-08 07:18:18 +0200

    run migrations

--- scripts/migrate.sql
+++ scripts/migrate.sql
@@ -1,7 +1,8 @@
-CREATE TABLE tasks IF not Exists (
+CREATE TABLE IF NOT EXISTS tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     note TEXT,
     status INT,
     due_on TIMESTAMP NULL DEFAULT NULL
 );
+
